3e7d488ea6ae1dc267f06cac840b73a2849d066e
Donald Mersiovsky <donald.mersiovsky@va.gov>
2019-03-06 07:51:15 -0600

    End of Section 4

---
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClientJsonpModule, HttpClient } from '@angular/common/http';
+import { typeWithParameters } from '@angular/compiler/src/render3/util';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
+  }
+ 
+
+}
--- DatingApp.API/readme.md
+++ DatingApp.API/readme.md
@@ -1,1 +1,1 @@
-this is a test application
+this is a test application test
